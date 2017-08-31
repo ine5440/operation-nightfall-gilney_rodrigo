4eef0f6b5e2a7ad9da6e587bd4960ab021dd0591
Rodrigo A. Costa <rodrigo.aguiar@grad.ufsc.br>
2017-08-31 16:45:17 -0300

    nothing to see here...

--- autotuner-prototype.py
+++ autotuner-prototype.py
@@ -6,27 +6,48 @@ import subprocess # to run stuff
 import sys # for args, in case you want them
 import time # for time
 
+
 def tuner(argv):
+    start = 1
+    n = 9
+    best_time = 99999
+    for value in range(start, n):
+        for var in range(4):
+            tempo = test(value, var, n)
+            if best_time > tempo:
+                best_var = var
+                index = value
+                best_time = tempo
+    print()
+    print(best_var, " - ", index, " - ", best_time)
+
+
+def test(value=1, var=0, n=9):
     exec_file = 'matmult'
-    compilation_line = ['gcc','-o',exec_file,'mm.c']
-    steps = ['-DSTEP=2']
+    compilation_line = ['gcc', '-o', exec_file, 'mm.c']
+    steps = ['-DSTEP=' + str(value)]
+    flags = ['-O' + str(var)]
 
     # Compile code
-    compilation_try = subprocess.run(compilation_line+steps)
+    compilation_try = subprocess.run(compilation_line+steps+flags)
     if (compilation_try.returncode == 0):
-        print("Happy compilation")
+        print("Happy compilation - ", value)
     else:
         print("Sad compilation")
 
     # Run code
-    input_size = str(4)
-    t_begin = time.time() # timed run
+    input_size = str(n)
+    t_begin = time.time()  # timed run
     run_trial = subprocess.run(['./'+exec_file, input_size])
     t_end = time.time()
     if (run_trial.returncode == 0):
-        print("Happy execution in "+str(t_end-t_begin))
+        tempo = t_end-t_begin
+        print("Happy execution in "+str(tempo))
+        return tempo
     else:
         print("Sad execution")
+    return
+
 
 
 if __name__ == "__main__":
